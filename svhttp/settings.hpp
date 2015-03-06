13369f000efae3abde33151672286ce240f93112
Jianyao <jianyaoy@gmail.com>
2015-03-06 21:47:48 +0800

    update svhttp/settings.hpp

--- svhttp/settings.hpp
+++ svhttp/settings.hpp
@@ -15,26 +15,26 @@ namespace svhttp
 	public:
 		http_options()
 		{ 
-			_options.clear(); 
+			options.clear(); 
 		}
 		~http_options()
 		{ 
-			_options.clear(); 
+			options.clear(); 
 		}
 
 		void insert(std::string &str_option)
 		{
-			_options.push_back(str_option);
+			options.push_back(str_option);
 		}
 		//bool insert(std::vector<std::string>& list_option);
 
-		std::vector<std::string> option_all() { return _options; };
+		std::vector<std::string> option_all() { return options; };
 
 		// 得到Header字符串.
 		std::string header_string() const
 		{
 			std::string str;
-			for (std::vector<std::string>::const_iterator f = _options.begin(); f != _options.end(); f++)
+			for (std::vector<std::string>::const_iterator f = options.begin(); f != options.end(); f++)
 			{
 				str += (*f + "\r\n");
 			}
@@ -42,7 +42,7 @@ namespace svhttp
 		}
 
 	private:
-		std::vector<std::string> _options;
+		std::vector<std::string> options;
 	};
 
 	typedef http_options request_header;
@@ -55,24 +55,24 @@ namespace svhttp
 		void set_proxy_type(curl_proxytype &type){ proxy_type = type; };
 		curl_proxytype get_proxy_type(){ return proxy_type; }
 
-		void set_hostname(const std::string& hostname){ _hostname = hostname; }
-		void set_port(const std::string& port) { _port = port; }
-		void set_username(const std::string &username) { _username = username; }
-		void set_password(const std::string &password) { _password = password; }
+		void set_hostname(const std::string& shostname){ hostname = shostname; }
+		void set_port(const std::string& sport) { port = sport; }
+		void set_username(const std::string &susername) { username = susername; }
+		void set_password(const std::string &spassword) { password = spassword; }
 
-		std::string get_hostname() { return _hostname; }
-		std::string get_port() { return _port; }
-		std::string get_username() { return _username; }
-		std::string get_password() { return _password; }
+		std::string get_hostname() { return hostname; }
+		std::string get_port() { return port; }
+		std::string get_username() { return username; }
+		std::string get_password() { return password; }
 
 	private:
 		curl_proxytype proxy_type;
 
-		std::string _hostname;
-		std::string _port;
+		std::string hostname;
+		std::string port;
 
-		std::string _username;
-		std::string _password;
+		std::string username;
+		std::string password;
 	};
 
 
